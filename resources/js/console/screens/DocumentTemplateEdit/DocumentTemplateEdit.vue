5a55e6a75d50effb5eb16db99ab8b8cbd08e2f05
Seth Phat <phattranminh96@gmail.com>
2023-04-30 11:22:50 +0700

    DocKing

--- resources/js/console/screens/DocumentTemplateEdit/DocumentTemplateEdit.vue
+++ resources/js/console/screens/DocumentTemplateEdit/DocumentTemplateEdit.vue
@@ -1,26 +1,22 @@
 <template>
-  <Card v-if="isLoading">
-    Loading template...
-  </Card>
+  <Card v-if="isLoading"> Loading template... </Card>
   <Card v-else-if="!isLoading && !template">
     <div class="flex flex-col">
       <span>Template is not exists</span>
       <div class="mt-2">
-        <Button @click="$router.replace({ name: 'document-templates-list' })">Go back to Document Templates List</Button>
+        <Button @click="$router.replace({ name: 'document-templates-list' })"
+          >Go back to Document Templates List</Button
+        >
       </div>
     </div>
   </Card>
   <Card v-else>
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
-        <h1
-          class="text-base font-semibold leading-6 text-gray-900"
-        >
+        <h1 class="text-base font-semibold leading-6 text-gray-900">
           Edit Document Template: {{ template.title }}
         </h1>
-        <p
-          class="mt-2 text-sm text-gray-700"
-        >
+        <p class="mt-2 text-sm text-gray-700">
           Configure the Document Template
         </p>
       </div>
@@ -30,10 +26,10 @@
 
 <script setup>
 import Card from '../../components/Card/Card.vue';
-import {onMounted, ref} from 'vue';
-import {useLoading} from "../../composable/useLoading";
-import {documentTemplateRepository} from "../../repositories/documentTemplate.repository";
-import Button from "../../components/Button/Button.vue";
+import { onMounted, ref } from 'vue';
+import { useLoading } from '../../composable/useLoading';
+import { documentTemplateRepository } from '../../repositories/documentTemplate.repository';
+import Button from '../../components/Button/Button.vue';
 
 const props = defineProps({
   uuid: {
