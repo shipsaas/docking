68598600e966504a86f033934b6e9a405e220ee0
Seth Phat <phattranminh96@gmail.com>
2023-04-30 09:48:35 +0700

    DocKing

--- resources/js/console/screens/DocumentTemplatesList/components/DeleteTemplateButton.vue
+++ resources/js/console/screens/DocumentTemplatesList/components/DeleteTemplateButton.vue
@@ -10,11 +10,26 @@
     :is-open="isOpenModal"
     title="Delete Confirmation"
   >
-    <p class="mt-4 text-sm text-gray-500">Are you sure you want to delete this template? This template will be marked as inactive and still available in the database.</p>
+    <p class="mt-4 text-sm text-gray-500">
+      Are you sure you want to delete this template? This template will be
+      marked as inactive and still available in the database.
+    </p>
     <template #bottom-buttons>
       <div class="flex gap-1">
-        <Button type="secondary" @click="isOpenModal = false" :disabled="isLoading">Cancel</Button>
-        <Button type="error" @click="onClickConfirmDelete" :disabled="isLoading">Delete</Button>
+        <Button
+          type="secondary"
+          @click="isOpenModal = false"
+          :disabled="isLoading"
+        >
+          Cancel
+        </Button>
+        <Button
+          type="error"
+          @click="onClickConfirmDelete"
+          :disabled="isLoading"
+        >
+          Delete
+        </Button>
       </div>
     </template>
   </Modal>
@@ -25,8 +40,8 @@ import Button from '../../../components/Button/Button.vue';
 import { documentTemplateRepository } from '../../../repositories/documentTemplate.repository';
 import { notify } from '@kyvg/vue3-notification';
 import { useLoading } from '../../../composable/useLoading';
-import Modal from "../../../components/Modal/Modal.vue";
-import {ref} from "vue";
+import Modal from '../../../components/Modal/Modal.vue';
+import { ref } from 'vue';
 
 const props = defineProps({
   template: {
