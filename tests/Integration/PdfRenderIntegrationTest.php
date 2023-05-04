4449530a31be21d657c086dc4a56b52cfbed65e7
Seth Phat <phattranminh96@gmail.com>
2023-05-04 13:57:49 +0700

    DocKing

---
+++ tests/Integration/PdfRenderIntegrationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Integration;
+
+use Tests\TestCase;
+
+class PdfRenderIntegrationTest extends TestCase
+{
+    public function testRenderPdfUsingGotenberg()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testRenderPdfUsingWkHtmlToPdf()
+    {
+        $this->assertTrue(true);
+    }
+}
