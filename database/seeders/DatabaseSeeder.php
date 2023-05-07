2cd14974b332385cdcca1a563dd353f6743225c7
Seth Phat <phattranminh96@gmail.com>
2023-05-07 23:58:51 +0700

    free templates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,49 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\GotenbergEngine;
+use App\Models\DocumentTemplate;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // seeds some test templates
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // 1. Sample HTML template
+        DocumentTemplate::create([
+            'key' => 'easy-template',
+            'category' => 'sample',
+            'title' => 'Easy Template',
+            'template' => 'Hello {{ $name }}, how are you today? <br /> I hope you are doing well.',
+            'default_variables' => [
+                'name' => 'Seth Phat',
+            ],
+            'metadata' => [
+                'driver' => 'gotenberg',
+                'engine' => GotenbergEngine::CHROMIUM->value,
+            ],
+        ]);
+
+        // 2. Invoice Template
+        DocumentTemplate::create([
+            'key' => 'invoice-template',
+            'category' => 'invoice-domain',
+            'title' => 'Invoice Template',
+            'template' => file_get_contents(
+                __DIR__ . '/../../tests/Integration/__fixtures__/invoice.html'
+            ),
+            'default_variables' => json_decode(
+                file_get_contents(
+                    __DIR__ . '/../../tests/Integration/__fixtures__/invoice.json'
+                ),
+                true
+            ),
+            'metadata' => [
+                'driver' => 'gotenberg',
+                'engine' => GotenbergEngine::CHROMIUM->value,
+            ],
+        ]);
     }
 }
