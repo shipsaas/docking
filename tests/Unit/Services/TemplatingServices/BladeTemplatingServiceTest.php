fabbea9eaf5f749d71ca20b731e4fbb1a2073879
Seth Phat <phattranminh96@gmail.com>
2023-09-17 23:36:23 +0700

    Translations tests

--- tests/Unit/Services/TemplatingServices/BladeTemplatingServiceTest.php
+++ tests/Unit/Services/TemplatingServices/BladeTemplatingServiceTest.php
@@ -3,15 +3,42 @@
 namespace Tests\Unit\Services\TemplatingServices;
 
 use App\Models\DocumentTemplate;
+use App\Models\Translation;
+use App\Models\TranslationGroup;
 use App\Services\TemplatingServices\BladeTemplatingService;
 use Tests\TestCase;
 
 class BladeTemplatingServiceTest extends TestCase
 {
-    public function testRenderReturnsTheRenderedHtml()
+    public function testRenderReturnsTheRenderedHtmlUseEnglishTranslations()
     {
+        $translationGroup = TranslationGroup::factory()->create([
+            'key' => 'base',
+        ]);
+        Translation::factory()->create([
+            'translation_group_id' => $translationGroup->uuid,
+            'key' => 'base.hello',
+            'name' => 'Hello',
+            'text' => [
+                'en' => 'Hello',
+                'vi' => 'Chao ban',
+            ],
+        ]);
+        Translation::factory()->create([
+            'translation_group_id' => $translationGroup->uuid,
+            'key' => 'base.hello.nice',
+            'name' => 'Nice',
+            'text' => [
+                'en' => 'Nice day to you',
+                'vi' => 'Chuc 1 ngay tot lanh',
+            ],
+        ]);
+
+        // English
+        $this->app->setLocale('en');
+
         $template = new DocumentTemplate([
-            'template' => 'Hello {{ $partner }}',
+            'template' => "@lang('base.hello') {{ \$partner }}, @lang('base.hello.nice')",
         ]);
 
         $service = new BladeTemplatingService();
@@ -19,7 +46,46 @@ class BladeTemplatingServiceTest extends TestCase
             'partner' => 'Seth',
         ]);
 
-        $this->assertSame('Hello Seth', $renderedHtml);
+        $this->assertSame('Hello Seth, Nice day to you', $renderedHtml);
+    }
+
+    public function testRenderReturnsTheRenderedHtmlUseVietnameseTranslations()
+    {
+        $translationGroup = TranslationGroup::factory()->create([
+            'key' => 'base',
+        ]);
+        Translation::factory()->create([
+            'translation_group_id' => $translationGroup->uuid,
+            'key' => 'base.hello',
+            'name' => 'Hello',
+            'text' => [
+                'en' => 'Hello',
+                'vi' => 'Chao ban',
+            ],
+        ]);
+        Translation::factory()->create([
+            'translation_group_id' => $translationGroup->uuid,
+            'key' => 'base.hello.nice.day',
+            'name' => 'Nice',
+            'text' => [
+                'en' => 'Nice day to you',
+                'vi' => 'Chuc 1 ngay tot lanh',
+            ],
+        ]);
+
+        // English
+        $this->app->setLocale('vi');
+
+        $template = new DocumentTemplate([
+            'template' => "@lang('base.hello') {{ \$partner }}, @lang('base.hello.nice.day')",
+        ]);
+
+        $service = new BladeTemplatingService();
+        $renderedHtml = $service->renderHtml($template, [
+            'partner' => 'Seth',
+        ]);
+
+        $this->assertSame('Chao ban Seth, Chuc 1 ngay tot lanh', $renderedHtml);
     }
 
     public function testRenderOnErrorReturnsTheErrorString()
